2d520b41426a4f2c6db0f1e2cb3217d0913379de
zibartsk <44426480+zibartsk@users.noreply.github.com>
2025-05-27 21:18:09 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -21,14 +21,14 @@ variable "sase_users" {
   default     = [] # Provide an empty list as a default, or remove if you always want to define it.
 }
 
-variable "app_registration_client_secret_duration" {
-  description = "The duration in hours for the client secret's validity (e.g., 8760 for 1 year, 4380 for 6 months)."
-  type        = number
-  default     = 8760 # Default to 1 year (8760 hours)
-}
-
 variable "sase_groups" {
   description = "A list of Azure AD group display names to assign to the SASE enterprise application."
   type        = list(string)
   default     = [] # Provide an empty list as a default, or make it required.
+}
+
+variable "app_registration_client_secret_duration" {
+  description = "The duration in hours for the client secret's validity (e.g., 8760 for 1 year, 4380 for 6 months)."
+  type        = number
+  default     = 8760 # Default to 1 year (8760 hours)
 }
